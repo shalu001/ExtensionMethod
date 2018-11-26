bdcadee5da5db1b79edc695aafda0ffa0e5ee00d
shalu001 <45366514+shalu001@users.noreply.github.com>
2018-11-26 23:00:20 +0530

    Added The First  Example

---
+++ ExtensionMethodProject/TestClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ExtensionMethodProject
+{
+    public class TestClass
+    {
+        public void Test1()
+        {
+            Console.WriteLine("Test 1 Method Of Test Class");
+        }
+
+        public void Test2()
+        {
+            Console.WriteLine("Test 2 Method Of Test Class");
+        }
+    }
+}
